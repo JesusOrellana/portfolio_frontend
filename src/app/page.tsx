42449b06370848ce956e7b993e07908028a331ba
JesusOrellana <58049396+JesusOrellana@users.noreply.github.com>
2025-06-13 13:29:18 -0400

    base project files

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Dashboard from '@/components/dashboard'
+import React from 'react'
+
+export default function Index() {
+    return (
+        <Dashboard />
+    )
+}
---
+++ src/app/ui/about/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Index() {
+    return (
+        <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+    )
+}
